00b0551201c92198ec71a40c86160800d49b3058
SergeantBulkin <bezdeneg.ya@yandex.ru>
2020-10-14 23:14:44 +0300

    Added DisposableManager to store subscriptions

--- app/src/main/java/by/sergeantbulkin/grpc/fragment/BaseFragment.java
+++ app/src/main/java/by/sergeantbulkin/grpc/fragment/BaseFragment.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,18 +13,24 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import by.sergeantbulkin.grpc.R;
+import by.sergeantbulkin.grpc.databinding.FragmentBaseBinding;
+import by.sergeantbulkin.grpc.model.DisposableManager;
 import by.sergeantbulkin.proto.MethodRequest;
 import by.sergeantbulkin.proto.MethodResponse;
-import by.sergeantbulkin.grpc.R;
 import by.sergeantbulkin.proto.RxmethodGrpc;
-import by.sergeantbulkin.grpc.databinding.FragmentBaseBinding;
 import by.sergeantbulkin.proto.methodGrpc;
+import dalvik.system.DexFile;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.reactivex.Single;
@@ -79,17 +87,19 @@ public class BaseFragment extends Fragment
             sendRxMessage();
         });
     }
-
+    //----------------------------------------------------------------------------------------------
     private void sendRxMessage()
     {
         binding.buttonRx.setEnabled(false);
         ManagedChannel channel = ManagedChannelBuilder.forAddress(HOST, PORT).usePlaintext().build();
         RxmethodGrpc.RxmethodStub stub = RxmethodGrpc.newRxStub(channel);
-        Single<MethodRequest> req = Single.just(MethodRequest.newBuilder().build());
-        Single<MethodResponse> resp = req.compose(stub::getMethodNumber);
-        resp.subscribeOn(Schedulers.io())
+
+        DisposableManager.add(Single
+                .just(MethodRequest.newBuilder().build())
+                .as(stub::getMethodNumber)
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new DisposableSingleObserver<MethodResponse>()
+                .subscribeWith(new DisposableSingleObserver<MethodResponse>()
                 {
                     @Override
                     public void onSuccess(MethodResponse methodResponse)
@@ -110,7 +120,8 @@ public class BaseFragment extends Fragment
                     {
                         e.printStackTrace();
                     }
-                });
+                }));
+
     }
     //----------------------------------------------------------------------------------------------
     private void sendMessage()
@@ -178,6 +189,13 @@ public class BaseFragment extends Fragment
             result.setText(s);
             button.setEnabled(true);
         }
+    }
+    //----------------------------------------------------------------------------------------------
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        DisposableManager.dispose();
     }
     //----------------------------------------------------------------------------------------------
 }
